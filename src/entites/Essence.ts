06a6d396f80b30e5b911fc19f293799ea5f4d599
Jtepssss <mateoelgamer125@gmail.com>
2024-11-09 15:58:10 -0500

    Entidades

---
+++ src/entites/Essence.ts
@@ -0,0 +1,24 @@
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+
+@Entity()
+export class Essence {
+    @PrimaryGeneratedColumn()
+    CodigoDelProducto!: number;
+    
+    @Column()
+    NombreDelProducto!: string;
+
+    @Column("text")
+    DetallesDelProducto!: string;
+  
+    @Column("text")
+    Ingredientes!: string;
+  
+    @Column("text")
+    Uso!: string;
+  
+    @Column("decimal")
+    Precio!: number;
+    
+
+}
